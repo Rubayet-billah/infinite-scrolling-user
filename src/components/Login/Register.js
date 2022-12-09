763f86d001d488e68a24282d910a3cc20b869748
Rubayet-billah <rubayetbillah909@gmail.com>
2022-12-10 04:13:42 +0600

    register and update profile implemented

--- src/components/Login/Register.js
+++ src/components/Login/Register.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const Register = () => {
-    return (
-        <div>
+    const { createUser, updateUserProfile } = useContext(AuthContext)
+    const { register, handleSubmit } = useForm();
+    const imgHostKey = process.env.REACT_APP_imgbb_apikey;
+    const navigate = useNavigate()
 
-        </div>
+    const handleRegister = (data) => {
+        const { name, email, password } = data
+
+        createUser(email, password)
+            .then(res => {
+                const user = res.user;
+                updateUserProfile(name)
+                    .then(() => {
+                        console.log(user)
+                        navigate('/')
+                    })
+                    .catch(err => console.error(err))
+            }).catch(err => console.error(err))
+
+    }
+    return (
+        <section className='md:h-[700px] flex justify-center items-center'>
+            <form onSubmit={handleSubmit(handleRegister)} className="card w-96 bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="text-2xl text-center">Register</h2>
+                    <label htmlFor="">Name</label>
+                    <input type="text" {...register('name')} placeholder="Name" className="input input-bordered w-full mb-2" />
+                    <label htmlFor="">Email</label>
+                    <input type="email" {...register('email')} placeholder="Email" className="input input-bordered w-full mb-2" />
+                    <label htmlFor="">Password</label>
+                    <input type="password" {...register('password')} placeholder="Password" className="input input-bordered w-full mb-2" />
+                    <div className="card-actions justify-end">
+                        <input type="submit" value='Register' className="btn btn-accent w-full" />
+                    </div>
+                </div>
+            </form>
+        </section>
     );
 };
 
--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from 'firebase/auth'
 import app from '../../firebase/firebase.config';
 
 
@@ -18,6 +18,11 @@ const AuthProvider = ({ children }) => {
         setLoading(true)
         return signInWithEmailAndPassword(auth, email, password);
     }
+    const updateUserProfile = (name) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name
+        })
+    }
 
     const logoutUser = () => {
         setLoading(true)
@@ -35,6 +40,7 @@ const AuthProvider = ({ children }) => {
     const authInfo = {
         user,
         createUser,
+        updateUserProfile,
         loginUser,
         logoutUser,
     }
