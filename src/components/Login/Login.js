bf5ff3084e2fd3754cfdfb4c3554be9de1281b0c
Rubayet-billah <rubayetbillah909@gmail.com>
2022-12-10 03:35:53 +0600

    login implemented

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const Login = () => {
-    return (
-        <div>
+    const { loginUser } = useContext(AuthContext)
+    const { register, handleSubmit } = useForm();
 
-        </div>
+    const handleLogin = (data) => {
+        const { email, password } = data;
+        loginUser(email, password)
+            .then(res => {
+                const user = res.user;
+                console.log(user)
+            }).catch(err => console.log(err))
+    }
+
+    return (
+        <section className='md:h-[700px] flex justify-center items-center'>
+            <form onSubmit={handleSubmit(handleLogin)} className="card w-96 bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="text-2xl text-center">Login</h2>
+                    <label htmlFor="">Email</label>
+                    <input type="email" {...register('email')} placeholder="Email" className="input input-bordered w-full mb-2" />
+                    <label htmlFor="">Password</label>
+                    <input type="password" {...register('password')} placeholder="Password" className="input input-bordered w-full" />
+                    <div className="card-actions justify-end">
+                        <input type="submit" value='Login' className="btn btn-accent w-full" />
+                    </div>
+                </div>
+            </form>
+        </section>
     );
 };
 
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -52,7 +52,7 @@ const Users = () => {
                 <section className='grid md:grid-cols-2 lg:grid-cols-3 gap-6'>
                     {
                         users?.map(user => <User
-                            key={user.id.value}
+                            key={user.login.uuid}
                             user={user}
                         ></User>)
                     }
